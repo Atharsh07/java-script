bd24da995498d408cebe91bfbeeb75a667b9be89
Atharsh07 <atharsh0425@gmail.com>
2024-01-03 15:45:01 +0530

    callback 2

--- Callback.js
+++ Callback.js
@@ -1,5 +1,17 @@
 
 //... callback function 
+/*
 document.getElementById("clickme").addEventListener("click", function xyz(){
     console.log("atharsh");
 })
+*/
+
+//.. qt we have to count how many times the button clicked .;-
+
+function attachEventlistner(){
+    let count = 0;
+    document.getElementById("clickme").addEventListener("click", function xyz(){
+        console.log("button clicked", ++count);
+    })
+}
+attachEventlistner();
