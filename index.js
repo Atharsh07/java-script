aba2fde323f01be6ba76866de65b2f0650eeade5
Atharsh07 <atharsh0425@gmail.com>
2024-01-03 10:42:04 +0530

    qt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function x(){
     var a = 7;
     function y(){
@@ -10,3 +11,39 @@ console.log(z);
 //.........
 
 z();
+//... so this is called clouser how this works 
+*/
+
+
+//... settimeout concept :----
+/*
+function x(){
+    var i =1; 
+    setTimeout(() => {
+        console.log(i);
+    }, 3000);
+    function z(){
+        var j = 2;
+        setTimeout(function(){
+            console.log(i,j);
+        }, 5000);
+        console.log("atharsh");
+    }
+    z();
+}
+x();
+console.log("Atharsh");
+*/
+
+//.. most asked intreview qt js..
+//.. print in console that 1 2 .. 5 after each of its time interval 
+
+function x(){
+    for(let i=1; i<=6; i++) {
+        setTimeout(function(){
+            console.log(i);
+        }, i*1000);
+    }
+}
+x();
+console.log("atharsh");
