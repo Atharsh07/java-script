c2418a13c52f11913164aedf3cf3502726694e1e
Atharsh07 <atharsh0425@gmail.com>
2024-01-03 11:46:33 +0530

    clouser advance

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ console.log("Atharsh");
 //.. most asked intreview qt js..
 //.. print in console that 1 2 .. 5 after each of its time interval 
 
+/*
 function x(){
     for(let i=1; i<=6; i++) {
         setTimeout(function(){
@@ -47,3 +48,67 @@ function x(){
 }
 x();
 console.log("atharsh");
+
+*/
+//this is the qt where we need to use var only not the let function --->
+/*
+function x(){
+    for(var i = 1; i<=5; i++){
+        function close(i){
+            setTimeout(function(){
+                console.log(i);
+            },i*1000);
+        }
+        close(i);
+    }
+}
+x()
+*/
+/* how the closur works behind the scene 
+function outest(){
+    var c = 20;
+    function outer(b){
+        function inner(){
+            console.log(a,b,c);
+        }
+        let a = 10;
+        return  inner;
+    }
+    return outer;
+}
+let a = 100;
+var close = outest()("atharsh");
+close();
+*/
+
+
+// data hiding and encapsulation (closur):----
+/*
+function counter(){
+    let count = 1;
+    return function incrementCounter(){
+        count++;
+        console.log(count);
+    }
+}
+var counter1 = counter();
+counter1();
+*/
+//.... contructive function
+
+function Counter(){
+    let count = 0;
+    this.incrementCounter = function(){
+        count++;
+        console.log(count);
+    }
+    this.decrementCounter = function(){
+        count--;
+        console.log(count);
+    }
+}
+var counter1 = new Counter();
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.decrementCounter();
+counter1.decrementCounter();
